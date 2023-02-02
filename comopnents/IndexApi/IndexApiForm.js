7e089d060bbfc1658ca1225886894ba2d315ba3e
MilonSarker40 <43253773+MilonSarker40@users.noreply.github.com>
2023-02-02 13:01:38 +0600

    checkbox link remove

--- comopnents/IndexApi/IndexApiForm.js
+++ comopnents/IndexApi/IndexApiForm.js
@@ -3,7 +3,7 @@ import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
-import DynamicCheckBox from '../DynamicCheckBox/DynamicCheckBox';
+
 
 const IndexApiForm = () => {
 
@@ -103,9 +103,6 @@ const IndexApiForm = () => {
                         {options}
                     </Form.Select>
                 </Form.Group> 
-            </Row>
-            <Row className='mb-3'>
-              <DynamicCheckBox />
             </Row>
             <Row className='mb-3'>
                <Form.Group as={Col} controlId="formStatus">
