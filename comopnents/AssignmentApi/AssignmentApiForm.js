60c86dcba7506f2eafdd022e60da4ac8f7395281
MilonSarker40 <43253773+MilonSarker40@users.noreply.github.com>
2023-02-07 15:42:31 +0600

    select item fix

--- comopnents/AssignmentApi/AssignmentApiForm.js
+++ comopnents/AssignmentApi/AssignmentApiForm.js
@@ -9,56 +9,61 @@ const IndexApiForm = ({selected}) => {
 
     console.log(selected)
 
-    // const [opt, setOpt] = useState([]);
+    const [opt, setOpt] = useState([]);
 
-    // const optd =[
-    //   {
-    //     id:1,
-    //     name:'select1',
-    //   },
-    //   {
-    //     id:2,
-    //     name:'select2',
-    //   },
-    //   {
-    //     id:3,
-    //     name:'select3',
-    //   }
-    // ]
+    const optd =[
+      {
+        id:1,
+        name:'select1',
+      },
+      {
+        id:2,
+        name:'select2',
+      },
+      {
+        id:3,
+        name:'select3',
+      }
+    ]
 
-    // useEffect(()=>{
-    //   setOpt(optd)
-    // },[])
+    useEffect(()=>{
+      setOpt(optd)
+    },[])
 
-    // const options =opt.map((value)=><option value={value.id}>{value.name}</option>)
+    const options =opt.map((value)=><option value={value.id}>{value.name}</option>)
 
   return (
     <>
       <div className='contact-form-wrp'>
          <Form action=''>
+            <div className='contact-form-check'>
             <Row className="mb-3">
-              <Col lg='6'>
-                <div className='contact-form-check'>
-                  <Form.Group className="mb-3" controlId="formBasicPassword">
-                    {
-                      selected && selected.map((selectItem,index) => (
-                        <>
-                        <Row className="mb-3">
-                          <Col lg='3'>
-                            <label>{selectItem}</label>
-                          </Col>
-                          <Col lg='9'>
-                            <Form.Control key={index} type="text" placeholder="Enter Value" />
-                          </Col>
-                          </Row>
-                        </>
-                      ))
-                    }
-                    
-                  </Form.Group>
-                </div>
-              </Col>
-            </Row>
+                <Form.Group as={Col} controlId="formStatus">
+                    <Form.Label>Status</Form.Label>
+                    <Form.Select aria-label="Default select example">
+                        <option>Select Status</option>
+                        {options}
+                    </Form.Select>
+                </Form.Group> 
+             </Row>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                {
+                  selected && selected.map((selectItem,index) => (
+                    <>
+                    <Row className="mb-3">
+                      <Col lg='2'>
+                        <label>{selectItem}</label>
+                      </Col>
+                      <Col lg='4'>
+                        <Form.Control key={index} type="text" placeholder="Enter Value" />
+                      </Col>
+                      </Row>
+                    </>
+                  ))
+                }
+                
+              </Form.Group>
+            </div>
             <div className='contact-submit'>
                 <Button gap={3} variant="primary" type="submit">
                     Save
