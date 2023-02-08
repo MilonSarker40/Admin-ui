fe62125b25e72dbab06ae782c2d61410a9794642
MilonSarker40 <43253773+MilonSarker40@users.noreply.github.com>
2023-02-08 17:34:32 +0600

    simulator from fix

--- comopnents/Simulator/SimulatorForm.js
+++ comopnents/Simulator/SimulatorForm.js
@@ -1,176 +1,177 @@
 import React, { useEffect, useState } from 'react';
+// import { useSelector } from 'react-redux';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 
 const SimulatorForm = () => {
+  const [mobile, setMobile] = useState("");
+  const [amount, setAmount] = useState(0.00);
+  const [country, setCountry] = useState(0);
+  const [network, setNetwork] = useState(0);
+  const [service, setService] = useState(0);
+  const [countries, setCountries] = useState([]);
+  const [networks, setNetworks] = useState([]);
+  const [services, setServices] = useState([]);
+  const [networklist, setNetworklist] = useState([]);
+  const [servicelist, setServicelist] = useState([]);
 
+  // const loginData = useSelector(state => state?.auth.loginData);
 
-    const [country, setCountry] = useState('');
-    const [network, setNetwork] = useState('');
-    const [serviceType, setServiceType] = useState('');
-    const [mobileNumber, setMobileNumber] = useState('');
-    const [amount, setAmount] = useState('');
-    const [opt, setOpt] = useState([]);
-    const [optN, setOptN] = useState([]);
-    const [optS, setOptS] = useState([]);
+  useEffect(() => {
+      fetch('http://localhost:3000/country/list')
+          .then((res) => res.json())
+          .then((data) => {
+              console.log("country : ", data.message);
+              setCountries(data.message);
+          })
+      fetch('http://localhost:3000/network/list')
+          .then((res) => res.json())
+          .then((data) => {
+              console.log("network : ", data.message);
+              setNetworks(data.message);
+          })
+      fetch('http://localhost:3000/service/list')
+          .then((res) => res.json())
+          .then((data) => {
+              console.log("service : ", data.message);
+              setServices(data.message);
+          })
+  }, [])
 
-    const optd=[
-      {
-        id:1,
-        name:'Bangladesh'
-      },
-      {
-        id:2,
-        name:'India'
-      },
-      {
-        id:3,
-        name:'Pak'
-      },
-    ]
+  const ctry = countries.map((value) => <option value={value.id}>{value.name}</option>)
+  const net = networklist.map((value) => <option value={value.id}>{value.name}</option>)
+  const serv = servicelist.map((value) => <option value={value.id}>{value.name}</option>)
 
-    useEffect(()=>{
-      setOpt(optd)
-    },[])
+  const mobileVal = (event) => {
+      setMobile(event.target.value);
+  }
 
-    const options =opt.map((value)=><option value={value.id}>{value.name}</option>)
+  const amountVal = (event) => {
+      setAmount(event.target.value);
+  }
 
-    const optdN=[
-      {
-        id:1,
-        name:'Gp'
-      },
-      {
-        id:2,
-        name:'Airtel'
-      },
-      {
-        id:3,
-        name:'Banglalink'
-      },
-    ]
+  const countryVal = (event) => {
+      console.log(event.target.value);
+      setCountry(event.target.value);
+      let ctry = event.target.value;
+      const filterdNetwork = networks.filter((data) => {
+          return data ? data.nationId == parseInt(ctry) : {}
+      });
+      console.log(filterdNetwork);
 
-    useEffect(()=>{
-      setOptN(optdN)
-    },[])
+      setNetworklist(filterdNetwork);
+  }
 
-    const options1 =optN.map((value)=><option value={value.id}>{value.name}</option>)
+  const networkVal = (event) => {
+      console.log(event.target.value);
+      setNetwork(event.target.value);
+      let net = event.target.value;
+      const filteredService = services.filter((data) => {
+          return data ? data.mobileId === parseInt(net) : {}
+      });
+      console.log(filteredService);
+      setServicelist(filteredService);
+  }
 
+  const serviceVal = (event) => {
+      setService(event.target.value);
+  }
 
-    const optdS=[
-      {
-        id:1,
-        name:'Dataload'
-      },
-      {
-        id:2,
-        name:'TopUp'
+  let data = {
+      'mobile': mobile,
+      'amount': amount,
+      'country': parseInt(country),
+      'network': parseInt(network),
+      'service': parseInt(service),
+      // 'user': loginData.username
+  }
+
+  const clearData = () => {
+      document.getElementById("mobile").value = "";
+      document.getElementById("amount").value = null;
+      document.getElementById("country").selectedIndex = "0";
+      document.getElementById("network").selectedIndex = "0";
+      document.getElementById("service").selectedIndex = "0";
+  }
+
+  const saveData = () => {
+      console.log(data);
+      if (mobile.length == 11){
+          fetch('http://localhost:3000/submitdata', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+          })
+          .then((response) => response.json())
+          .then((data) => {
+              console.log('Success:', data);
+              alert(data.message.msg);
+          })
+          .catch((error) => {
+              console.error('Error:', error);
+              alert(error.message.msg);
+          });
+          clearData();
+      }else{
+          alert(`you have entered a number with the lenth of ${mobile.length}`);
       }
-    ]
-
-    useEffect(()=>{
-      setOptS(optdS)
-    },[])
-
-    const options2 =optS.map((value)=><option value={value.id}>{value.name}</option>)
-
-    const [allValue, setAllValue] = useState([]);
-
-    const formSubmit=(e)=>{
-        e.preventDefault()
-        const newValue ={country:country, network:network, serviceType:serviceType, mobileNumber:mobileNumber, amount:amount}
-
-        setAllValue([...allValue, newValue])
-        console.log(newValue)
-
-        setCountry('')
-        setNetwork('') 
-        setServiceType('') 
-        setMobileNumber('')
-        setAmount('')
-    }
+      
+  }
 
   return (
     <>
      <div className='contact-form-wrp'>
-         <Form action='' onSubmit={formSubmit}>
+         <Form action=''>
            <Row className="mb-3">
                <Form.Group as={Col} controlId="formCountry">
                     <Form.Label>Country</Form.Label>
-                    <Form.Select aria-label="Default select example" value={country} onChange={(e)=>setCountry(e.target.value)}>
-                        <option>Select Country</option>
-                        {options}
+                    <Form.Select aria-label="Default select example" id="country" onChange={countryVal}>
+                        <option value='0'>Select Country</option>
+                        {ctry}
                     </Form.Select>
                 </Form.Group>
                 <Form.Group as={Col} controlId="formNetwork">
                     <Form.Label>Network</Form.Label>
-                    <Form.Select aria-label="Default select example" value={network} onChange={(e)=>setNetwork(e.target.value)}>
-                        <option>Select Network</option>
-                        {options1}
+                    <Form.Select aria-label="Default select example" id="network" onChange={networkVal}>
+                        <option value="0">Select Network</option>
+                        {net}
                     </Form.Select>
                 </Form.Group>
             </Row>
             <Row className="mb-3">
                 <Form.Group as={Col} controlId="formCountry">
                     <Form.Label>Service Type</Form.Label>
-                    <Form.Select aria-label="Default select example" value={serviceType} onChange={(e)=>setServiceType(e.target.value)}>
-                        <option>Select Service</option>
-                        {options2}
+                    <Form.Select aria-label="Default select example" id="service" onChange={serviceVal}>
+                        <option value="0">Select Service</option>
+                        {serv}
                     </Form.Select>
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="formMobileNumber">
                     <Form.Label>Mobile Number</Form.Label>
-                    <Form.Control type="text" placeholder="Mobile Number" value={mobileNumber} onChange={(e)=>setMobileNumber(e.target.value)} />
+                    <Form.Control type="text" placeholder="Mobile Number" id="mobile" onChange={mobileVal} />
                 </Form.Group>
             </Row>
             <Row className='mb-3'>
                <Form.Group as={Col} controlId="formAmount">
                     <Form.Label>Amount</Form.Label>
-                    <Form.Control type="text" placeholder="Enter Amount" value={amount} onChange={(e)=>setAmount(e.target.value)} />
+                    <Form.Control type="text" placeholder="Enter Amount" step="0.01" id="amount" onChange={amountVal} />
                 </Form.Group>
             </Row>
             <div className='contact-submit'>
-                <Button gap={3} variant="primary" type="submit">
-                    Save
+                <Button gap={3} variant="primary" type="submit" id="btn_save" onClick={saveData}>
+                  Recharge
                 </Button>
-                <Button variant="primary" type="submit">
+                <Button variant="primary" type="submit" id="btn_clear" onClick={clearData}>
                     Clear
                 </Button>
             </div>
             </Form>
       </div>
-      <section className='show-data'>
-       {allValue.map((currentValue) => {
-
-          const { country, network, serviceType, mobileNumber, amount} = currentValue
-
-          return (
-            <>
-              <div className='sign-box'>
-                <h1>Send Successfully</h1>
-                <h3>
-                  Country : <span>{country}</span>
-                </h3>
-                <h3>
-                  Network : <span>{network}</span>
-                </h3>
-                <h3>
-                  Service Type : <span>{serviceType}</span>
-                </h3>
-                <h3>
-                    Mobile Number : <span>{mobileNumber}</span>
-                </h3>
-                <h3>
-                    Amount : <span>{amount}</span>
-                </h3>
-              </div>
-            </>
-          )
-        })}
-      </section>
     </>
   )
 }
